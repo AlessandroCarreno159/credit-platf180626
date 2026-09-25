60767501ca4f3a4a37b8bc3e966c2716db787d3c
AlessandroCarreno159 <alessandro_carreno@usmp.pe>
2026-09-25 00:31:22 -0500

    feat: Crear modelo MVC -Crear modelo MVC con login Individual como base del proyecto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace credit_platf.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
